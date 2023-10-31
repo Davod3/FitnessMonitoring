2884017ebcf38448ff172ac5ff818b6978be5c37
Davod3 <davidpereira.pereira4@gmail.com>
2023-10-31 22:48:07 +0000

    Working mqtt message comsumption in spring

--- processor/src/main/java/iot/project/processor/ProcessorApplication.java
+++ processor/src/main/java/iot/project/processor/ProcessorApplication.java
@@ -2,12 +2,59 @@ package iot.project.processor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.core.MessageProducer;
+import org.springframework.integration.mqtt.core.MqttPahoClientFactory;
+import org.springframework.integration.mqtt.inbound.MqttPahoMessageDrivenChannelAdapter;
+import org.springframework.integration.mqtt.support.DefaultPahoMessageConverter;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessageHandler;
+import org.springframework.messaging.MessagingException;
 
 @SpringBootApplication
 public class ProcessorApplication {
 
+	private static final String MQTT_TOPIC = "idc/fc56339";
+	private static final String MQTT_CLIENT = "processor";
+	private static final String MQTT_HOSTNAME = "172.100.10.10:1883";
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(ProcessorApplication.class, args);
 	}
+
+	@Bean
+	public MessageChannel mqttInputChannel() {
+		return new DirectChannel();
+	}
+
+	@Bean
+	public MessageProducer inbound() {
+		MqttPahoMessageDrivenChannelAdapter adapter =
+				new MqttPahoMessageDrivenChannelAdapter("tcp://" + MQTT_HOSTNAME,
+						MQTT_CLIENT, MQTT_TOPIC);
+
+		adapter.setCompletionTimeout(5000);
+		adapter.setConverter(new DefaultPahoMessageConverter());
+		adapter.setQos(1);
+		adapter.setOutputChannel(mqttInputChannel());
+		return adapter;
+	}
+
+	@Bean
+	@ServiceActivator(inputChannel = "mqttInputChannel")
+	public MessageHandler handler() {
+		return new MessageHandler() {
+			@Override
+			public void handleMessage(Message<?> message) throws MessagingException {
+				System.out.println(message.getPayload());
+			}
+		};
+	}
+
+
 
 }
