fb39389a47b783b023a9761aa6bab79609c750e1
Davod3 <davidpereira.pereira4@gmail.com>
2023-12-04 12:01:32 +0000

    Modified client to update timestamps

--- client/client1.py
+++ client/client1.py
@@ -21,48 +21,49 @@ client.connect(broker_hostname, port)
 client.loop_start()
 
 
-msg = []
-
 # Read data
 datafile = open("client/online.data", "r")
 
+# Get lines
 lines = datafile.readlines()
 
-# for each line, separate by ';' and put all lines in msg
-for line in lines[1:]:
-
-    # Get current date and time
-    curr = datetime.datetime.now()
-
-    curr_date = curr.strftime("%d/%m/%y")
-    curr_time = curr.strftime("%H-%M-%S-%f")
-
-    fields = line.strip().split(';')
-    msg.append({
-        "date":curr_date,
-        "time":curr_time,
-        "acceleration_x":fields[0],
-        "acceleration_y":fields[1],
-        "acceleration_z":fields[2],
-        "gyro_x":fields[3],
-        "gyro_y":fields[4],
-        "gyro_z":fields[5]
-    })
-
-
 # Send data to Broker every 1 seconds (send a line)
-msg_count = 0
 try:
-    while msg_count < len(msg):
-        time.sleep(1)
-        payload = json.dumps(msg[msg_count])
-        result = client.publish(topic, payload)
-        status = result[0]
-        if status == 0:
-            print("Message "+ str(payload) + " is published to topic " + topic)
-        else:
-            print("Failed to send message to topic " + topic)
-        msg_count += 1
+    while True:
+        
+        for line in lines[1:]:
+
+            # Get current date and time
+            curr = datetime.datetime.now()
+
+            # Format date and time
+            curr_date = curr.strftime("%d/%m/%y")
+            curr_time = curr.strftime("%H-%M-%S-%f")
+
+            fields = line.strip().split(';')
+
+            msg = {
+                    "date":curr_date,
+                    "time":curr_time,
+                    "acceleration_x":fields[0],
+                    "acceleration_y":fields[1],
+                    "acceleration_z":fields[2],
+                    "gyro_x":fields[3],
+                    "gyro_y":fields[4],
+                    "gyro_z":fields[5]
+                }
+
+
+            time.sleep(1)
+            payload = json.dumps(msg)
+            result = client.publish(topic, payload)
+            status = result[0]
+            if status == 0:
+                print("Message "+ str(payload) + " is published to topic " + topic)
+            else:
+                print("Failed to send message to topic " + topic)
+
+
 finally:
     client.loop_stop()
 
